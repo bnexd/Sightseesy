fde9df846ce7e731ab2fb658e12aaf39c1f2299a
andrethiele <thielea@th-brandenburg.de>
2019-12-12 11:52:26 +0100

    (Android) : Update

--- Android-App/app/src/main/java/com/tomtom/router/dagger/Injector.kt
+++ Android-App/app/src/main/java/com/tomtom/router/dagger/Injector.kt
@@ -1,7 +1,6 @@
 package com.tomtom.router.dagger
 
 import com.tomtom.router.App
-import com.tomtom.router.ui.trips.SelectedTripActivity
 import com.tomtom.router.ui.trips.TripsFragment
 
 fun inject(fragment: TripsFragment) {
@@ -9,12 +8,4 @@ fun inject(fragment: TripsFragment) {
         .getMainComponentFactory()
         .create(fragment.requireActivity())
         .inject(fragment)
-}
-
-
-fun inject(activity: SelectedTripActivity) {
-    App.appComponent(activity)
-        .getMainComponentFactory()
-        .create(activity)
-        .inject(activity)
 }
